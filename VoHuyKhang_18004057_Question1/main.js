4322908ca0526c71249ed59c84c3c85ce2e2a2e8
1 + 1 = 3 <47880923+ANTU-ALT-F4@users.noreply.github.com>
2023-01-07 16:52:02 -0800

    Add files via upload

---
+++ VoHuyKhang_18004057_Question1/main.js
@@ -0,0 +1,63 @@
+function ovRose(){
+    document.getElementById('click1').style.color = "red";
+    document.getElementById('click1').style.fontSize = "30px";
+    document.getElementById('img').src = './Web Design/Flowers/rose.jpg';
+    document.getElementById('img').style.width = "100%";
+    document.getElementById('img').style.height = "250px";
+}
+
+function otRose(){
+    document.getElementById('click1').style.color = "blue";
+    document.getElementById('click1').style.fontSize = "20px";
+    document.getElementById('img').src = '';
+    document.getElementById('img').style.width = "0px";
+    document.getElementById('img').style.height = "00px";
+}
+// -----------------------------------------------------------
+function ovTulips(){
+    document.getElementById('click2').style.color = "red";
+    document.getElementById('click2').style.fontSize = "30px";
+    document.getElementById('img').src = './Web Design/Flowers/tulip.jpg';
+    document.getElementById('img').style.width = "100%";
+    document.getElementById('img').style.height = "250px";
+}
+
+function otTulips(){
+    document.getElementById('click2').style.color = "blue";
+    document.getElementById('click2').style.fontSize = "20px";
+    document.getElementById('img').src = '';
+    document.getElementById('img').style.width = "0px";
+    document.getElementById('img').style.height = "00px";
+}
+// -----------------------------------------------------------
+function ovOrchid(){
+    document.getElementById('click3').style.color = "red";
+    document.getElementById('click3').style.fontSize = "30px";
+    document.getElementById('img').src = './Web Design/Flowers/orchid.jpg';
+    document.getElementById('img').style.width = "100%";
+    document.getElementById('img').style.height = "250px";
+}
+
+function otOrchid(){
+    document.getElementById('click3').style.color = "blue";
+    document.getElementById('click3').style.fontSize = "20px";
+    document.getElementById('img').src = '';
+    document.getElementById('img').style.width = "0px";
+    document.getElementById('img').style.height = "00px";
+}
+// -----------------------------------------------------------
+function ovSunflower(){
+    document.getElementById('click4').style.color = "red";
+    document.getElementById('click4').style.fontSize = "30px";
+    document.getElementById('img').src = './Web Design/Flowers/flower.png';
+    document.getElementById('img').style.width = "100%";
+    document.getElementById('img').style.height = "250px";
+}
+
+function otSunflower(){
+    document.getElementById('click4').style.color = "blue";
+    document.getElementById('click4').style.fontSize = "20px";
+    document.getElementById('img').src = '';
+    document.getElementById('img').style.width = "0px";
+    document.getElementById('img').style.height = "00px";
+}
